d6e92adb18bd11a8e8df05b0332be9a309238841
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-24 15:53:39 +0600

    completed feed and add post

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import { Box, Stack } from "@mui/material";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Add from "./components/Add";
 import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
 import Rightbar from "./components/Rightbar";
 import Sidebar from "./components/Sidebar";
 
-const App = () => {
+function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box bgcolor={"background.default"} color={"text.primary"}>
-      <Navbar />
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
         <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar/>
+          <Sidebar setMode={setMode} mode={mode} />
           <Feed />
           <Rightbar />
         </Stack>
-    </Box>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
