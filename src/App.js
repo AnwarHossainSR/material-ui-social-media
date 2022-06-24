c7007dc8055f7f6c06e25a678b2901668f6cc812
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-24 11:55:29 +0600

    set folder structure and setup layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Box, Stack } from "@mui/material";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Box bgcolor={"background.default"} color={"text.primary"}>
+      <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar/>
+          <Feed />
+          <Rightbar />
+        </Stack>
+    </Box>
+  );
+};
 
-export default App
+export default App;
